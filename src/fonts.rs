f46b2da4702fa9761b4f289d55f22a2562a9ece7
Yaroslav Malykh <fisuri@murena.io>
2024-08-07 16:35:36 +0400

    add match weight

--- src/fonts.rs
+++ src/fonts.rs
@@ -1,23 +1,40 @@
 use std::borrow::Cow;
 use std::sync::OnceLock;
 
-pub static UI_FONT: Font = Font::new();
+use iced::font::{self, Weight};
+
+pub static UI_FONT: Font = Font::new(Weight::Medium);
 
 #[derive(Debug, Clone)]
 pub struct Font {
+    weight: Weight,
     inner: OnceLock<iced::Font>,
 }
 
 impl Font {
-    const fn new() -> Self {
+    const fn new(weight: font::Weight) -> Self {
         Self {
+            weight,
             inner: OnceLock::new(),
         }
     }
 
     fn set(&self, name: String) {
         let name = Box::leak(name.into_boxed_str());
+        let weight = match &self.weight {
+            Weight::Thin => Weight::Thin,
+            Weight::ExtraLight => Weight::ExtraLight,
+            Weight::Light => Weight::Light,
+            Weight::Normal => Weight::Normal,
+            Weight::Medium => Weight::Medium,
+            Weight::Semibold => Weight::Semibold,
+            Weight::Bold => Weight::Bold,
+            Weight::ExtraBold => Weight::ExtraBold,
+            Weight::Black => Weight::Black,
+        };
+
         let _ = self.inner.set(iced::Font {
+            weight,
             ..iced::Font::with_name(name)
         });
     }
@@ -30,9 +47,11 @@ impl From<Font> for iced::Font {
 }
 
 pub fn set() {
-    UI_FONT.set("JetBrains Mono Medium".to_string())
+    UI_FONT.set("JetBrains Mono".to_string())
 }
 
 pub fn load() -> Vec<Cow<'static, [u8]>> {
-    vec![include_bytes!("../fonts/JetBrainsMono-Medium.ttf").as_slice().into()]
+    vec![include_bytes!("../fonts/JetBrainsMono-Medium.ttf")
+        .as_slice()
+        .into()]
 }
