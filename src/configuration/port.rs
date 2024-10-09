df7894b5d8ef497d960e192fe6fc7d76a4f10a5e
Yaroslav Malykh <fisuri@murena.io>
2024-10-09 23:35:20 +0400

    configurations: communicating with Keypad has been updated [2/2]
    The write_keypad_port and read_keypad_port (it has become significantly shorter) functions have been updated

--- src/configuration/port.rs
+++ src/configuration/port.rs
@@ -1,53 +1,92 @@
 use std::time::Duration;
 
+use tokio_serial::SerialPort;
+
 use super::{command::command_to_string, ARRAY_LEN};
 
-pub fn get_keypad_port() -> String {
-    // Порт Linux /dev/ttyACM0
-    // Порт Windows COM3
-    // let ports = serialport::available_ports().expect("No ports found!");
+// pub fn get_keypad_port() {
+//     // Порт Linux /dev/ttyACM0
+//     // Порт Windows COM3
+//     let ports = serialport::available_ports().expect("No ports found!");
 
-    // let port = serialport::new("/dev/ttyACM0", 115200)
-    //     .timeout(Duration::from_millis(10))
-    //     .open()
-    //     .expect("Failed to open port");
-    "/dev/ttyACM0".to_string()
+//     let write_data_array: [u16; ARRAY_LEN] = [11, 0, 0, 0, 0, 0, 0, 0, 0];
+
+//     for i in ports.iter() {
+//         let port_name = &i.port_name;
+//         let port = serialport::new(port_name, 115200)
+//             .timeout(Duration::from_millis(10))
+//             .open()
+//             .expect("Failed to open port");
+
+//         // let port_data = async {
+//         //     write_keypad_port(port.try_clone(), write_data_array).await;
+//         //     read_keypad_port(port.try_clone()).await
+//         // };
+
+//         println!("{:#?}", port)
+//     }
+
+//     // let port = serialport::new("/dev/ttyACM0", 115200)
+//     //     .timeout(Duration::from_millis(10))
+//     //     .open()
+//     //     .expect("Failed to open port");
+
+//     // "/dev/ttyACM0".to_string()
+// }
+
+pub async fn get_keypad_port() -> Vec<String> {
+    let mut results = Vec::new();
+
+    let ports = tokio_serial::available_ports().expect("No ports found!");
+    let write_data_array: [u16; ARRAY_LEN] = [101, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    for port in ports.iter() {
+        let port_name = &port.port_name;
+        let port = match tokio_serial::new(port_name, 115_200)
+            .timeout(Duration::from_millis(10))
+            .open()
+        {
+            Ok(port) => port,
+            Err(err) => {
+                eprintln!("Failed to open port {}: {}", port_name, err);
+                continue;
+            }
+        };
+
+        write_keypad_port(port.try_clone(), write_data_array).await;
+        let read_result = read_keypad_port(port.try_clone()).await;
+
+        results.push(format!("{}: {}", port_name, read_result));
+    }
+
+    println!("{:#?}", results);
+
+    results
 }
 
-pub async fn write_keypad_port(port: String, write_data_array: [u16; ARRAY_LEN]) {
-    // let pirt = get_keypad_port().await;
+pub async fn write_keypad_port(
+    port: Result<Box<dyn SerialPort>, tokio_serial::Error>,
+    write_data_array: [u16; ARRAY_LEN],
+) -> String {
     let write_data = command_to_string(&write_data_array).await;
 
-    let mut command = serialport::new(&port, 115200)
-        .timeout(Duration::from_millis(10))
-        .open()
-        .expect("Failed to open port");
-
-    command
+    port.unwrap()
         .write_all(write_data.as_bytes())
         .expect("Write failed!");
 
-    read_keypad_port(port).await;
+    String::new()
 }
 
-pub async fn read_keypad_port(port: String) {
-    let mut serial_buf: Vec<u8> = vec![0; 20];
+pub async fn read_keypad_port(port: Result<Box<dyn SerialPort>, tokio_serial::Error>) -> String {
+    let mut serial_buf = vec![0; 256];
 
-    let mut port = serialport::new(port, 115200)
-        .timeout(Duration::from_millis(10))
-        .open()
-        .expect("Failed to open port");
-
-    port.read(serial_buf.as_mut_slice())
+    port.unwrap()
+        .read(serial_buf.as_mut_slice())
         .expect("Found no data!");
+    let buf = String::from_utf8(serial_buf).unwrap();
+    let trimmed_buf = buf.trim_end_matches(|c: char| c.is_control()).to_string();
 
-    let ints: Vec<u8> = serial_buf.into_iter().filter(|&b| b != 59).collect();
-    let buf_string = String::from_utf8(ints).unwrap();
-    let parts: Vec<&str> = buf_string.split(',').collect();
-    let buf_u16 = parts
-        .into_iter()
-        .map(|x| x.parse::<u16>().unwrap())
-        .collect::<Vec<u16>>();
+    println!("{:?}", trimmed_buf);
 
-    println!("{:?}", buf_u16);
+    trimmed_buf
 }
