1856c6625b0aa89736aaecd6164212b8699766bc
Yaroslav Malykh <fisuri@murena.io>
2024-10-17 00:10:52 +0400

    ports: functions of interaction with the serial port have been updated

--- src/configuration/port.rs
+++ src/configuration/port.rs
@@ -1,92 +1,147 @@
 use std::time::Duration;
 
-use tokio_serial::SerialPort;
+use regex::Regex;
+use tokio_serial::{available_ports, new, SerialPort};
 
 use super::{command::command_to_string, ARRAY_LEN};
 
-// pub fn get_keypad_port() {
-//     // Порт Linux /dev/ttyACM0
-//     // Порт Windows COM3
-//     let ports = serialport::available_ports().expect("No ports found!");
-
-//     let write_data_array: [u16; ARRAY_LEN] = [11, 0, 0, 0, 0, 0, 0, 0, 0];
-
-//     for i in ports.iter() {
-//         let port_name = &i.port_name;
-//         let port = serialport::new(port_name, 115200)
-//             .timeout(Duration::from_millis(10))
-//             .open()
-//             .expect("Failed to open port");
-
-//         // let port_data = async {
-//         //     write_keypad_port(port.try_clone(), write_data_array).await;
-//         //     read_keypad_port(port.try_clone()).await
-//         // };
-
-//         println!("{:#?}", port)
-//     }
-
-//     // let port = serialport::new("/dev/ttyACM0", 115200)
-//     //     .timeout(Duration::from_millis(10))
-//     //     .open()
-//     //     .expect("Failed to open port");
-
-//     // "/dev/ttyACM0".to_string()
-// }
-
-pub async fn get_keypad_port() -> Vec<String> {
-    let mut results = Vec::new();
-
-    let ports = tokio_serial::available_ports().expect("No ports found!");
+#[cfg(target_os = "linux")]
+pub async fn get_keypad_port() -> String {
+    let mut result = String::new();
+    let mut vec_ports_str = Vec::new();
+    let ports = available_ports().expect("No ports found!");
     let write_data_array: [u16; ARRAY_LEN] = [101, 0, 0, 0, 0, 0, 0, 0, 0];
+    let response_data_array: [u16; ARRAY_LEN] = [101, 0, 0, 0, 0, 0, 0, 0, 1];
+    let regex = Regex::new(r"/dev/ttyACM\d+").unwrap();
 
-    for port in ports.iter() {
-        let port_name = &port.port_name;
-        let port = match tokio_serial::new(port_name, 115_200)
+    for port in ports.into_iter() {
+        let port_name = port.port_name;
+        vec_ports_str.push(port_name);
+    }
+    let vec_ports_str: Vec<&str> = vec_ports_str.iter().map(|s| s.as_str()).collect();
+    let filtered_ports: Vec<String> = vec_ports_str
+        .into_iter()
+        .filter(|port_str| regex.is_match(port_str))
+        .map(|s| s.to_string())
+        .collect();
+
+    for port_name in filtered_ports.iter() {
+        let port = match new(port_name, 115_200)
             .timeout(Duration::from_millis(10))
             .open()
         {
             Ok(port) => port,
-            Err(err) => {
-                eprintln!("Failed to open port {}: {}", port_name, err);
+            Err(_err) => {
+                // eprintln!("Ошибка открытия порта {}: {}", port_name, err);
                 continue;
             }
         };
 
-        write_keypad_port(port.try_clone(), write_data_array).await;
-        let read_result = read_keypad_port(port.try_clone()).await;
+        match write_keypad_port(port.try_clone(), write_data_array).await {
+            Ok(_) => {
+                match read_keypad_port(port.try_clone()).await {
+                    Ok(_) => {
+                        result.push_str(port_name);
+                    }
+                    Err(_err) => {
+                        // println!("Ошибка чтения из порта {}: {}", port_name, err);
+                        continue;
+                    }
+                };
+            }
+            Err(_err) => {
+                // eprintln!("Ошибка записи в порт {}: {}", port_name, err);
+                continue;
+            }
+        };
 
-        results.push(format!("{}: {}", port_name, read_result));
+        // println!("{:#?}", port_name)
+    }
+    // println!("{:#?}", result);
+    result
+}
+
+#[cfg(target_os = "windows")]
+pub async fn get_keypad_port() -> String {
+    let mut result = String::new();
+    let mut vec_ports_str: Vec<String> = Vec::new();
+    let ports = available_ports().expect("No ports found!");
+    let write_data_array: [u16; ARRAY_LEN] = [101, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    for port in ports.into_iter() {
+        let port_name = port.port_name;
+        vec_ports_str.push(port_name);
     }
 
-    println!("{:#?}", results);
+    let filtered_ports: Vec<String> = vec_ports_str
+        .into_iter()
+        .filter(|port| !port.contains("COM1"))
+        .collect();
 
-    results
+    for port_name in filtered_ports.iter() {
+        let port = match new(port_name, 115_200)
+            .timeout(Duration::from_millis(10))
+            .open()
+        {
+            Ok(port) => port,
+            Err(_err) => {
+                // eprintln!("Ошибка открытия порта {}: {}", port_name, err);
+                continue;
+            }
+        };
+
+        match write_keypad_port(port.try_clone(), write_data_array).await {
+            Ok(_) => {
+                let _read_result = match read_keypad_port(port.try_clone()).await {
+                    Ok(_) => result.push_str(port_name),
+                    Err(_err) => {
+                        // println!("Ошибка чтения из порта {}: {}", port_name, err);
+                        continue;
+                    }
+                };
+            }
+            Err(_err) => {
+                // eprintln!("Ошибка записи в порт {}: {}", port_name, err);
+                continue;
+            }
+        };
+
+        // println!("{:#?}", port_name)
+    }
+    // println!("{:#?}", result);
+    result
 }
 
 pub async fn write_keypad_port(
     port: Result<Box<dyn SerialPort>, tokio_serial::Error>,
     write_data_array: [u16; ARRAY_LEN],
-) -> String {
+) -> Result<String, tokio_serial::Error> {
     let write_data = command_to_string(&write_data_array).await;
 
-    port.unwrap()
+    port?
         .write_all(write_data.as_bytes())
         .expect("Write failed!");
 
-    String::new()
+    Ok(String::new())
 }
 
-pub async fn read_keypad_port(port: Result<Box<dyn SerialPort>, tokio_serial::Error>) -> String {
+pub async fn read_keypad_port(
+    port: Result<Box<dyn SerialPort>, tokio_serial::Error>,
+) -> Result<String, tokio_serial::Error> {
     let mut serial_buf = vec![0; 256];
 
-    port.unwrap()
-        .read(serial_buf.as_mut_slice())
-        .expect("Found no data!");
-    let buf = String::from_utf8(serial_buf).unwrap();
-    let trimmed_buf = buf.trim_end_matches(|c: char| c.is_control()).to_string();
+    match port?.read(serial_buf.as_mut_slice()) {
+        Ok(_m) => {
+            let buf = String::from_utf8(serial_buf).unwrap();
+            let trimmed_buf = buf.trim_end_matches(|c: char| c.is_control()).to_string();
+            let ok_trimmed_buf = Ok(trimmed_buf);
 
-    println!("{:?}", trimmed_buf);
-
-    trimmed_buf
+            println!("{:?}", ok_trimmed_buf);
+            ok_trimmed_buf
+        }
+        Err(err) => {
+            // eprintln!("Ошибка чтения из порта: {}", err);
+            Err(tokio_serial::Error::from(err))
+        }
+    }
 }
