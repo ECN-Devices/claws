c3574422736e7c3206c76cfece489937e6f1d05a
Yaroslav Malykh <fisuri@murena.io>
2024-10-30 01:10:38 +0400

    tests: temporary deletion of tests

--- src/tests/command.rs
+++ src/tests/command.rs
@@ -1,19 +1,19 @@
-#[cfg(test)]
-mod test_command_to_string {
-    use tokio::runtime::Builder;
+// #[cfg(test)]
+// mod test_command_to_string {
+//     use tokio::runtime::Builder;
 
-    use crate::configuration::command::command_to_string;
+//     use crate::configuration::command::command_to_string;
 
-    #[test]
-    fn positive_numbers() {
-        let runtime = Builder::new_current_thread().enable_all().build().unwrap();
+//     #[test]
+//     fn positive_numbers() {
+//         let runtime = Builder::new_current_thread().enable_all().build().unwrap();
 
-        let array: [u16; 9] = [11, 0, 10, 0, 100, 0, 4, 0, 0];
-        let result = runtime.block_on(command_to_string(&array));
+//         let array: [u16; 9] = [11, 0, 10, 0, 100, 0, 4, 0, 0];
+//         let result = runtime.block_on(command_to_string(&array));
 
-        assert_eq!(result, "11,0,10,0,100,0,4,0,0;")
-    }
-}
+//         assert_eq!(result, "11,0,10,0,100,0,4,0,0;")
+//     }
+// }
 
 // #[cfg(test)]
 // mod test_get_keypad_port {
