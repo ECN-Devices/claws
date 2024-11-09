ba93823bfe1d58197ebbffadf32ff36b74edf11c
Yaroslav Malykh <fisuri@murena.io>
2024-11-09 18:39:45 +0400

    tests: restoring tests

--- src/tests/command.rs
+++ src/tests/command.rs
@@ -1,34 +1,16 @@
-// #[cfg(test)]
-// mod test_command_to_string {
-//     use tokio::runtime::Builder;
+#[cfg(test)]
+mod test_command_to_string {
+    use tokio::runtime::Builder;
 
-//     use crate::configuration::command::command_to_string;
+    use crate::configuration::command::command_to_string;
 
-//     #[test]
-//     fn positive_numbers() {
-//         let runtime = Builder::new_current_thread().enable_all().build().unwrap();
+    #[test]
+    fn to_string() {
+        let runtime = Builder::new_current_thread().enable_all().build().unwrap();
 
-//         let array: [u16; 9] = [11, 0, 10, 0, 100, 0, 4, 0, 0];
-//         let result = runtime.block_on(command_to_string(&array));
+        let array: [u16; 9] = [11, 0, 10, 0, 100, 0, 4, 0, 0];
+        let result = runtime.block_on(command_to_string(&array));
 
-//         assert_eq!(result, "11,0,10,0,100,0,4,0,0;")
-//     }
-// }
-
-// #[cfg(test)]
-// mod test_get_keypad_port {
-//     use tokio::runtime::Builder;
-
-//     use crate::configuration::{command_to_string, ARRAY_LEN};
-
-//     #[test]
-//     fn keypad_port() {
-//         let runtime = Builder::new_current_thread().enable_all().build().unwrap();
-
-//         runtime.block_on({
-//             let write_data_array: [u16; ARRAY_LEN] = [101, 0, 0, 0, 0, 0, 0, 0, 0];
-//             let write_data = command_to_string(&write_data_array).await;
-//             write_data
-//         })
-//     }
-// }
+        assert_eq!(result, "11,0,10,0,100,0,4,0,0;")
+    }
+}
--- src/tests/config.rs
+++ src/tests/config.rs
@@ -1,9 +1,10 @@
 #[cfg(test)]
 mod test_config_dir {
-    use std::env::set_var;
+    use log::error;
+    use std::{env::set_var, fs};
     use tokio::runtime::Builder;
 
-    use crate::configuration::config::get_config_dir;
+    use crate::configuration::config::{create_config_dir, get_config_dir};
 
     #[test]
     fn test_get_config_dir_linux() {
@@ -11,12 +12,12 @@ mod test_config_dir {
 
         let runtime = Builder::new_current_thread().enable_all().build().unwrap();
 
-        let config_file_path = runtime.block_on(get_config_dir());
+        let config_path = runtime.block_on(get_config_dir());
 
         let username = whoami::username();
-        let result = format!("/home/{}/.config/Lapa", username);
+        let result = format!("/home/{}/.config/Claws", username);
 
-        assert_eq!(config_file_path.display().to_string(), result)
+        assert_eq!(config_path.display().to_string(), result)
     }
 
     #[cfg(windows)]
@@ -25,33 +26,68 @@ mod test_config_dir {
         set_var("OS", "windows");
         let runtime = Builder::new_current_thread().enable_all().build().unwrap();
 
-        let config_file_path = runtime.block_on(get_config_dir());
+        let config_path = runtime.block_on(get_config_dir());
 
         let username = whoami::username();
-        let result = format!("C:/Users/{}/Documents/Lapa", username);
+        let result = format!("C:/Users/{}/Documents/Claws", username);
 
-        assert_eq!(config_file_path.display().to_string(), result)
+        assert_eq!(config_path.display().to_string(), result)
     }
 
-    // #[test]
+    #[test]
     fn test_create_config_dir_linux() {
-        todo!()
+        set_var("OS", "linux");
+
+        let runtime = Builder::new_current_thread().enable_all().build().unwrap();
+        let config_path = runtime.block_on(get_config_dir());
+
+        if config_path.exists() {
+            if let Err(e) = fs::remove_dir_all(config_path.clone()) {
+                error!("Ошибка удаления папки конфигурации: {}", e);
+            };
+        }
+
+        let result = runtime.block_on(create_config_dir());
+
+        assert!(result.is_ok());
+
+        if let Err(e) = fs::remove_dir_all(config_path) {
+            error!("Ошибка удаления папки конфигурации: {}", e);
+        };
     }
 
     #[cfg(windows)]
-    // #[test]
+    #[test]
     fn test_create_config_dir_windows() {
-        todo!()
+        set_var("OS", "windows");
+
+        let runtime = Builder::new_current_thread().enable_all().build().unwrap();
+        let config_path = runtime.block_on(get_config_dir());
+
+        if let Err(e) = fs::remove_dir_all(config_path.clone()) {
+            error!("Ошибка удаления папки конфигурации: {}", e);
+        };
+
+        let result = runtime.block_on(create_config_dir());
+
+        assert!(result.is_ok());
+
+        if let Err(e) = fs::remove_dir_all(config_path) {
+            error!("Ошибка удаления папки конфигурации: {}", e);
+        };
     }
 }
 
 #[cfg(test)]
 mod test_config_file {
 
-    use std::env::set_var;
+    use log::error;
+    use std::{env::set_var, fs};
     use tokio::runtime::Builder;
 
-    use crate::configuration::config::get_config_file;
+    use crate::configuration::config::{
+        create_config_dir, create_config_file, get_config_dir, get_config_file,
+    };
 
     #[test]
     fn test_get_config_file_linux() {
@@ -61,7 +97,7 @@ mod test_config_file {
         let config_file_path = runtime.block_on(get_config_file());
 
         let username = whoami::username();
-        let result = format!("/home/{}/.config/Lapa/lapa.toml", username);
+        let result = format!("/home/{}/.config/Claws/claws.toml", username);
 
         assert_eq!(config_file_path.display().to_string(), result)
     }
@@ -76,12 +112,59 @@ mod test_config_file {
         let config_file_path = runtime.block_on(get_config_file());
 
         let username = whoami::username();
-        let result = format!("C:/Users/{}/Documents/Lapa/lapa.toml", username);
+        let result = format!("C:/Users/{}/Documents/Claws/claws.toml", username);
 
         assert_eq!(config_file_path.display().to_string(), result)
     }
 
-    fn test_create_config_file() {
-        todo!()
+    #[test]
+    fn test_create_config_file_linux() {
+        set_var("OS", "linux");
+
+        let runtime = Builder::new_current_thread().enable_all().build().unwrap();
+        let config_dir_path = runtime.block_on(get_config_dir());
+        let config_file_path = runtime.block_on(get_config_file());
+        if config_file_path.exists() {
+            if let Err(e) = fs::remove_file(config_file_path.clone()) {
+                error!("Ошибка удаления файла конфигурации: {}", e);
+            };
+        }
+
+        let result = runtime.block_on(async {
+            create_config_dir().await?;
+            create_config_file().await
+        });
+
+        assert!(result.is_ok());
+
+        if let Err(e) = fs::remove_dir_all(config_dir_path) {
+            error!("Ошибка удаления файла конфигурации: {}", e);
+        };
+    }
+
+    #[cfg(windows)]
+    #[test]
+    fn test_create_config_file_windows() {
+        set_var("OS", "windows");
+
+        let runtime = Builder::new_current_thread().enable_all().build().unwrap();
+        let config_dir_path = runtime.block_on(get_config_dir());
+        let config_file_path = runtime.block_on(get_config_file());
+        if config_file_path.exists() {
+            if let Err(e) = fs::remove_file(config_file_path.clone()) {
+                error!("Ошибка удаления файла конфигурации: {}", e);
+            };
+        }
+
+        let result = runtime.block_on(async {
+            create_config_dir().await?;
+            create_config_file().await
+        });
+
+        assert!(result.is_ok());
+
+        if let Err(e) = fs::remove_dir_all(config_dir_path) {
+            error!("Ошибка удаления файла конфигурации: {}", e);
+        };
     }
 }
