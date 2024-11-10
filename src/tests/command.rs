b50a4cb50324ab8a39ad066bf85064ea223b4c7c
Yaroslav Malykh <fisuri@murena.io>
2024-11-10 15:55:59 +0400

    tests: extending the tests of the command_to_string function

--- src/tests/command.rs
+++ src/tests/command.rs
@@ -1,12 +1,31 @@
 #[cfg(test)]
 mod test_command_to_string {
-    use crate::configuration::command::command_to_string;
+    use crate::configuration::{command::command_to_string, ARRAY_LEN};
 
     #[tokio::test]
-    async fn to_string() {
-        let array: [u16; 9] = [11, 0, 10, 0, 100, 0, 4, 0, 0];
-        let result = command_to_string(&array).await;
 
-        assert_eq!(result, "11,0,10,0,100,0,4,0,0;")
+    async fn test_command_to_string_normal() {
+        let array: [u16; ARRAY_LEN] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let result = command_to_string(&array).await;
+        assert_eq!(result, "1,2,3,4,5,6,7,8,9;");
+    }
+
+    #[tokio::test]
+
+    async fn test_command_to_string_with_zeros() {
+        let array: [u16; ARRAY_LEN] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+        let result = command_to_string(&array).await;
+        assert_eq!(result, "0,0,0,0,0,0,0,0,0;");
+    }
+
+    #[tokio::test]
+
+    async fn test_command_to_string_with_max_values() {
+        let array: [u16; ARRAY_LEN] = [u16::MAX; ARRAY_LEN];
+        let result = command_to_string(&array).await;
+        assert_eq!(
+            result,
+            "65535,65535,65535,65535,65535,65535,65535,65535,65535;"
+        );
     }
 }
