6ff83fcc2a691503d09942c6df57182bd89401fa
Yaroslav Malykh <fisuri@murena.io>
2024-11-17 00:38:17 +0400

    command: optimization of the function

--- src/configuration/command.rs
+++ src/configuration/command.rs
@@ -1,17 +1,12 @@
 use super::ARRAY_LEN;
 
-pub async fn command_to_string(array: &[u16; 9]) -> String {
-    let mut command_string = String::new();
-
-    for (i, &value) in array.iter().take(ARRAY_LEN).enumerate() {
-        command_string.push_str(&value.to_string());
-
-        if i + 1 != ARRAY_LEN {
-            command_string.push(',')
-        } else {
-            command_string.push(';')
-        }
-    }
+pub async fn command_to_string(array: &[u16; ARRAY_LEN]) -> String {
+    let command_string = array
+        .iter()
+        .map(|&value| value.to_string())
+        .collect::<Vec<String>>()
+        .join(",")
+        + ";";
 
     command_string
 }
