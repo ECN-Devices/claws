3fa6d67868026d1343b7d1bd2e25c6023a7bcbe7
Yaroslav Malykh <fisuri@murena.io>
2024-11-19 01:49:47 +0400

    tests: removing unnecessary lines

--- src/tests/command.rs
+++ src/tests/command.rs
@@ -3,7 +3,6 @@ mod test_command_to_string {
     use crate::configuration::{command::command_to_string, ARRAY_LEN};
 
     #[tokio::test]
-
     async fn test_command_to_string_normal() {
         let array: [u16; ARRAY_LEN] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
         let result = command_to_string(&array).await;
@@ -11,7 +10,6 @@ mod test_command_to_string {
     }
 
     #[tokio::test]
-
     async fn test_command_to_string_with_zeros() {
         let array: [u16; ARRAY_LEN] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
         let result = command_to_string(&array).await;
@@ -19,7 +17,6 @@ mod test_command_to_string {
     }
 
     #[tokio::test]
-
     async fn test_command_to_string_with_max_values() {
         let array: [u16; ARRAY_LEN] = [u16::MAX; ARRAY_LEN];
         let result = command_to_string(&array).await;
