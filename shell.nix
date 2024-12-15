6348c79b3e360c7a33348ab15120768f1a2e1980
Yaroslav Malykh <fisuri@murena.io>
2024-12-15 17:50:20 +0400

    init nix

---
+++ shell.nix
@@ -0,0 +1,25 @@
+{ pkgs ? import <nixpkgs> { } }:
+let 
+  libPath = with pkgs; lib.makeLibraryPath [
+    libGL
+    libxkbcommon
+    wayland
+  ];
+in
+pkgs.mkShell {
+  # Get dependencies from the main package
+  inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
+  # Additional tooling
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+    systemd
+  ];
+
+  buildInputs = with pkgs; [
+    libGL
+    libxkbcommon
+    wayland
+  ];
+
+  LD_LIBRARY_PATH = libPath;
+}
