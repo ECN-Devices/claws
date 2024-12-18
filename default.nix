3e9dca9d91a2e813ae36f31cbe8de607b4f92de1
Yaroslav Malykh <fisuri@murena.io>
2024-12-19 00:01:25 +0400

    nix: format with alejandra

--- default.nix
+++ default.nix
@@ -1,28 +1,27 @@
-{
-  pkgs ? import <nixpkgs> { },
-}:
-let
+{pkgs ? import <nixpkgs> {}}: let
   manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
 in
-pkgs.rustPlatform.buildRustPackage rec {
-  pname = manifest.name;
-  version = manifest.version;
-  cargoLock.lockFile = ./Cargo.lock;
-  src = pkgs.lib.cleanSource ./.;
+  pkgs.rustPlatform.buildRustPackage rec {
+    pname = manifest.name;
+    version = manifest.version;
+    cargoLock.lockFile = ./Cargo.lock;
+    src = pkgs.lib.cleanSource ./.;
 
-  buildInputs = with pkgs; [
-    pkg-config
-    systemd
-    libGL
-    libxkbcommon
-    wayland
-  ];
+    buildInputs = with pkgs; [
+      pkg-config
+      systemd
+      libGL
+      libxkbcommon
+      wayland
+    ];
 
-  nativeBuildInputs = with pkgs; [
-    pkg-config
-    systemd
-    libGL
-    libxkbcommon
-    wayland
-  ];
-}
+    nativeBuildInputs = with pkgs; [
+      pkg-config
+      systemd
+      libGL
+      libxkbcommon
+      wayland
+    ];
+
+    doCheck = false;
+  }
--- flake.nix
+++ flake.nix
@@ -3,19 +3,19 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
-  outputs =
-    { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgsFor = nixpkgs.legacyPackages;
-    in
-    {
-      packages = forAllSystems (system: {
-        default = pkgsFor.${system}.callPackage ./default.nix { };
-      });
-      devShells = forAllSystems (system: {
-        default = pkgsFor.${system}.callPackage ./shell.nix { };
-      });
-    };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    pkgsFor = nixpkgs.legacyPackages;
+  in {
+    packages = forAllSystems (system: {
+      default = pkgsFor.${system}.callPackage ./default.nix {};
+    });
+    devShells = forAllSystems (system: {
+      default = pkgsFor.${system}.callPackage ./shell.nix {};
+    });
+  };
 }
