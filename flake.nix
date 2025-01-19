27b6094f11659dd33009babfb5cb4ef4449583fb
Yaroslav Malykh <fisuri@murena.io>
2025-01-20 00:41:05 +0400

    flake: use flake-utils

--- flake.nix
+++ flake.nix
@@ -1,23 +1,39 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    flake-utils,
+    rust-overlay,
     ...
-  } @ inputs: let
-    supportedSystems = ["x86_64-linux"];
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    pkgs = nixpkgs.legacyPackages;
-  in {
-    packages = forAllSystems (system: {
-      default = pkgs.${system}.callPackage ./default.nix {};
-    });
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        # supportedSystems = ["x86_64-linux"];
+        # forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        # pkgs = nixpkgs.legacyPackages;
+      in {
+        packages = {
+          default = pkgs.callPackage ./default.nix {};
+        };
 
-    devShells = forAllSystems (system: {
-      default = pkgs.${system}.callPackage ./shell.nix {};
-    });
-  };
+        devShells = {
+          default = pkgs.callPackage ./shell.nix {};
+        };
+      }
+    );
 }
