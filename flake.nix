9b5e5e008b62f63505ca9b595e90324f200b32cc
Yaroslav Malykh <fisuri@murena.io>
2025-05-08 17:42:58 +0400

    flake: update pre-commit

--- flake.nix
+++ flake.nix
@@ -9,8 +9,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    pre-commit-env = {
-      url = "github:chenow/nix-pre-commit";
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -26,7 +26,6 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        pre-commit-lib = inputs.pre-commit-env.lib.${system};
       in {
         packages = {
           default = pkgs.callPackage ./nix/default.nix {inherit pkgs;};
@@ -34,22 +33,84 @@
 
         formatter = pkgs.alejandra;
 
+        checks.pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          enabledPackages = with pkgs; [
+            pkg-config
+            systemd
+          ];
+          hooks = {
+            alejandra = {
+              enable = true;
+              package = pkgs.alejandra;
+              args = ["-q"];
+            };
+            deadnix = {
+              enable = true;
+              args = ["-e" "-q"];
+            };
+            statix = {
+              enable = true;
+              args = ["fix"];
+            };
+
+            check-json.enable = true;
+            pretty-format-json.enable = true;
+
+            check-yaml.enable = true;
+            yamlfmt = {
+              enable = true;
+              package = pkgs.yaml-language-server;
+            };
+
+            check-toml.enable = true;
+            taplo = {
+              enable = true;
+              package = pkgs.taplo;
+            };
+
+            cargo-check.enable = true;
+            rustfmt = {
+              enable = true;
+              packageOverrides = {
+                cargo = pkgs.rust-bin.stable.latest.default;
+                rustfmt = pkgs.rust-bin.stable.latest.default;
+              };
+            };
+            clippy = {
+              enable = true;
+              settings.allFeatures = true;
+              packageOverrides = {
+                cargo = pkgs.rust-bin.stable.latest.default;
+                clippy = pkgs.rust-bin.stable.latest.default;
+              };
+            };
+
+            trim-trailing-whitespace.enable = true;
+            check-merge-conflicts.enable = true;
+          };
+        };
+
         devShells = {
           default = pkgs.callPackage ./nix/shell.nix {};
-          git = pre-commit-lib.mkDevShell {
-            extraPackages = with pkgs; [
-              alejandra
-              taplo
-              cargo-nextest
-              pkg-config
-              systemd
-              (rust-bin.stable.latest.default.override {
-                extensions = [
-                  "cargo"
-                ];
-              })
-            ];
+          git = pkgs.mkShell {
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+            buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
           };
+          # git_test = pre-commit-lib.mkDevShell {
+          #   extraPackages = with pkgs; [
+          #     alejandra
+          #     taplo
+          #     cargo-nextest
+          #     pkg-config
+          #     systemd
+          #     (rust-bin.stable.latest.default.override {
+          #       extensions = [
+          #         "cargo"
+          #       ];
+          #     })
+          #   ];
+          # };
         };
       }
     );
