f7f56f432785cbcbbfc85822edc9df54804db273
Yaroslav Malykh <fisuri@murena.io>
2025-07-06 22:37:52 +0400

    flake: add windows-gnu

--- flake.nix
+++ flake.nix
@@ -39,6 +39,7 @@
             ];
             targets = [
               "x86_64-unknown-linux-gnu"
+              "x86_64-pc-windows-gnu"
               "x86_64-pc-windows-msvc"
             ];
           }
