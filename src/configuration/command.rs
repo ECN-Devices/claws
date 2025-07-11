0144fc6d09b90ede1521cbdff9b8d7d792e15369
Yaroslav Malykh <fisuri@murena.io>
2025-07-11 16:09:35 +0400

    configuration: update command

--- src/configuration/command.rs
+++ src/configuration/command.rs
@@ -1,12 +1,10 @@
 use super::ARRAY_LEN;
 
 pub async fn command_to_string(array: &[u16; ARRAY_LEN]) -> String {
-    let command_string = array
+    array
         .iter()
         .map(|&value| value.to_string())
-        .collect::<Vec<String>>()
+        .collect::<Vec<_>>()
         .join(",")
-        + ";";
-
-    command_string
+        + ";"
 }
