2091e82e96f4067e01ca29e396c32c8f7324b2c0
Yaroslav Malykh <fisuri@murena.io>
2025-08-06 17:02:51 +0400

    chore: restore profile reading and writing

--- src/hardware/serial/profile.rs
+++ src/hardware/serial/profile.rs
@@ -1,22 +1,20 @@
-use super::{DeviceIO, Keypad, KeypadError};
+use super::Keypad;
 use crate::{
   data::{
     Config,
     profiles::{KEYPAD_BUTTONS, Profile},
   },
-  hardware::buffers::Buffers,
+  hardware::{
+    buffers::{Buffers, BuffersIO},
+    commands::{Value, profile, stick, switch},
+  },
 };
-use log::error;
 use anyhow::Result;
+use log::debug;
 use serialport::SerialPort;
-use std::{
-  sync::{Arc, Mutex},
-  thread::sleep,
-  time::Duration,
-};
+use std::sync::{Arc, Mutex};
 
-/// Трейт для работы с профилями кейпада через последовательный порт
-pub trait SerialProfile {
+impl Keypad {
   /**
   Читает текущий профиль с устройства через последовательный порт
   # Аргументы
@@ -25,14 +23,23 @@ pub trait SerialProfile {
   # Возвращает
   Прочитанный профиль или ошибку
   */
-  fn receive_profile(port: &mut Arc<Mutex<Box<dyn SerialPort>>>, buffers: &mut Buffers) -> Profile;
+  pub fn profile_receive(buffers: &mut Buffers) -> Result<Profile> {
+    let mut keypad_profile = Profile::default();
 
-  /**
-  Сохраняет текущий профиль с устройства в хранилище
-  # Аргументы
-  * `port` - Ссылка на последовательный порт
-  */
-  fn save_profile_file(port: &mut Arc<Mutex<Box<dyn SerialPort>>>, buffers: &mut Buffers);
+    // Читаем имя профиля
+    let profile_name = String::from_utf8_lossy(&profile::request_name(buffers)?).into_owned();
+    keypad_profile.name = profile_name;
+
+    // Читаем конфигурацию кнопок (1..16)
+    let buttons_s = switch::request_code_ascii(buffers)?;
+    keypad_profile.buttons = buttons_s;
+
+    // Читаем конфигурацию стика (4 направления)
+    let stick_s = stick::request_position_ascii(buffers).unwrap_or([b'?'; 4]);
+    keypad_profile.stick = stick_s;
+
+    Ok(keypad_profile)
+  }
 
   /**
   Записывает профиль на устройство через последовательный порт
@@ -40,103 +47,44 @@ pub trait SerialProfile {
   * `port` - Ссылка на последовательный порт (`Arc<Mutex>` для потокобезопасности)
   * `profile` - Профиль для записи
   */
-  fn send_profile(
-    port: &mut Arc<Mutex<Box<dyn SerialPort>>>,
-    profile: Profile,
-    buffers: &mut Buffers,
-  ) -> Result<()>;
-}
-
-impl SerialProfile for Keypad {
-  fn receive_profile(port: &mut Arc<Mutex<Box<dyn SerialPort>>>, buffers: &mut Buffers) -> Profile {
-    let mut keypad_profile = Profile::default();
-
-    // Читаем конфигурацию кнопок (1..16)
-    (1..=KEYPAD_BUTTONS).for_each(|button_num| {
-      let _ = Keypad::send(port, buffers);
-
-      // Задержка между запросами
-      // Иначе не читает??????
-      sleep(Duration::from_millis(10));
-
-      match Keypad::receive(port) {
-        Ok(mut buf) => {
-          buf.drain(0..2);
-          let button_data: [u8; 6] = buf.try_into().unwrap();
-          keypad_profile.buttons[button_num as usize - 1] = button_data
-        }
-        Err(e) => error!("Ошибка чтения: stick {button_num}; с ошибкой: {e}"),
-      }
-    });
-
-    // Читаем конфигурацию стика (4 направления)
-    (1usize..=4).for_each(|stick_num| {
-      // Keypad::send(port, buffers);
-
-      // Задержка между запросами
-      // Иначе не читает??????
-      sleep(Duration::from_millis(10));
-
-      match Keypad::receive(port) {
-        Ok(buf) => keypad_profile.stick[stick_num - 1] = buf[stick_num],
-        Err(e) => error!("Ошибка чтения: stick {stick_num}; с ошибкой: {e}"),
-      }
-    });
-
-    // Читаем имя профиля
-    let _ = Keypad::send(port, buffers);
-
-    // Задержка между запросами
-    // Иначе не читает??????
-    sleep(Duration::from_millis(10));
-
-    match Keypad::receive(port) {
-      Ok(buf) => keypad_profile.name = String::from_utf8_lossy(&buf).trim_start().into(),
-      Err(e) => error!("Ошибка чтения: name; с ошибкой: {e}"),
-    }
-
-    keypad_profile
-  }
-
-  fn save_profile_file(port: &mut Arc<Mutex<Box<dyn SerialPort>>>, buffers: &mut Buffers) {
-    let keypad_profile = Self::receive_profile(port, buffers);
-    keypad_profile.save()
-  }
-
-  fn send_profile(
-    port: &mut Arc<Mutex<Box<dyn SerialPort>>>,
-    profile: Profile,
-    buffers: &mut Buffers,
-  ) -> Result<()> {
-    // Записываем конфигурацию кнопок
-    // (1..=KEYPAD_BUTTONS).for_each(|i| {
-    //   Keypad::send(
-    //     port,
-    //     &KeypadCommands::Swtich(switch::Command::SetCodeASCII(
-    //       i,
-    //       profile.buttons[i as usize - 1],
-    //     )),
-    //   )
-    //   .unwrap()
-    // });
-    for _ in 1..=KEYPAD_BUTTONS {
-      Keypad::send(port, buffers)?
-    }
-
-    // Записываем конфигурацию стика
-    for _ in 1..=4 {
-      Keypad::send(port, buffers)?
-    }
-
-    // Записываем имя профиля
-    let mut name = [0u8; 15];
+  pub fn profile_send(buffers: &mut Buffers, profile: Profile) -> Result<()> {
+    let mut profile_name = [0u8; 15];
     profile
       .name
       .chars()
-      .take(name.len())
+      .take(profile_name.len())
       .enumerate()
-      .for_each(|(i, c)| name[i] = c as u8);
+      .for_each(|(i, c)| profile_name[i] = c as u8);
 
-    Keypad::send(port, buffers)
+    let switch_s = profile.buttons;
+    let stick_s = profile.stick;
+
+    // Записываем имя профиля
+    buffers
+      .send()
+      .push(profile::Command::SetName(profile_name).get());
+
+    // Записываем конфигурацию кнопок
+    (1..=KEYPAD_BUTTONS).for_each(|i| {
+      buffers
+        .send()
+        .push(switch::Command::SetCodeASCII(i, switch_s[i as usize - 1]).get());
+    });
+
+    // Записываем конфигурацию стика
+    (1..=4).for_each(|i| {
+      buffers
+        .send()
+        .push(stick::Command::SetPositionASCII(i, stick_s[i as usize - 1]).get());
+    });
+
+    Ok(())
+  }
+
+  /**
+  Сохраняет текущий профиль с устройства в хранилище
+  */
+  pub fn save_profile_file(profile: Profile) {
+    profile.save()
   }
 }
