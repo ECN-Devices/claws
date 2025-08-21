72f61047b003baf488000b61f22c362f04e6fd58
Yaroslav Malykh <fisuri@murena.io>
2025-08-21 11:44:53 +0400

    feat(buttons): add intermediate fields of the structure

--- src/hardware/serial/buttons.rs
+++ src/hardware/serial/buttons.rs
@@ -2,4 +2,21 @@
 pub struct KeypadButton {
   pub id: usize,
   pub label: String,
+  pub vec_str: Vec<String>,
+  pub code: Vec<u8>,
 }
+impl KeypadButton {
+  pub fn reduce_label(element: &str) -> String {
+    match element {
+      "Control" => "Ctrl".to_string(),
+      "Delete" => "Del".to_string(),
+      "Escape" => "Esc".to_string(),
+      "ArrowUp" => "↑".to_string(),
+      "ArrowRight" => "→".to_string(),
+      "ArrowDown" => "↓".to_string(),
+      "ArrowLeft" => "←".to_string(),
+      _ => element.to_string(),
+    }
+  }
+}
+
