c00c9517521768bdd457560c92654788d80e0e46
Yaroslav Malykh <fisuri@murena.io>
2025-10-16 18:24:48 +0400

    chore: alphabetical error sorting

--- src/errors/serial.rs
+++ src/errors/serial.rs
@@ -3,24 +3,24 @@
 /// Перечень возможных ошибок при работе с Serial и буферами
 #[derive(Debug, thiserror::Error)]
 pub enum KeypadError {
-  #[error("No serial ports found")]
-  NoPortsFound,
-
   #[error("Send buffer is empty")]
   BufferEmpty,
 
   #[error("Invalid packet format")]
   InvalidPacketFormat,
 
-  #[error("Serial port error: {0}")]
-  SerialError(#[from] serialport::Error),
+  #[error("IO error: {0}")]
+  IoError(#[from] std::io::Error),
 
   #[error("Mutex lock error: {0}")]
   LockError(String),
 
+  #[error("No serial ports found")]
+  NoPortsFound,
+
   #[error("No Response")]
   NoResponse(Vec<u8>),
 
-  #[error("IO error: {0}")]
-  IoError(#[from] std::io::Error),
+  #[error("Serial port error: {0}")]
+  SerialError(#[from] serialport::Error),
 }
